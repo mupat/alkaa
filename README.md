3a217ca2a04e9407914c18b450ef19fe52b851e0
Patrick Müller <dev@mupat.net>
2014-05-14 12:58:29 +0200

    add mac installation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-dev
-===
+#dev env
+
+## use on mac
+- install sublime
+- start sublime
+- run `bootstrap_mac.sh`
+- install font [`Source Code Pro`](https://github.com/adobe/Source-Code-Pro)
---
+++ bootstrap_mac.sh
@@ -0,0 +1,46 @@
+#!/bin/sh 
+
+### variables
+APP=$HOME/Library/Application\ Support
+SUBLIME=$APP/Sublime\ Text\ 2/Packages
+
+GREEN="\x1B[;032m"
+NC="\x1B[0m"
+### commands
+
+#set bash_profile from repo
+echo "${GREEN}set bash_profile${NC}"
+rm "$HOME/.bash_profile"
+ln -s "$PWD/bash_profile" "$HOME/.bash_profile"
+
+#set tmxu config from repo
+echo "${GREEN}set tmux config${NC}"
+rm "$HOME/.tmux.conf"
+ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+
+#install homebrew
+echo "${GREEN}install homebrew${NC}"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew update
+
+#install tmux
+echo "${GREEN}install tmux${NC}"
+brew install tmux
+
+#install node
+echo "${GREEN}install nvm${NC}"
+git clone https://github.com/creationix/nvm.git $HOME/.nvm
+source $HOME/.nvm/nvm.sh
+
+echo "${GREEN}install node 0.10${NC}"
+nvm install 0.10
+
+#bootstrap sublime
+echo "${GREEN}bootstrap sublime${NC}"
+#make sure to start sublime at least one time
+sudo rm -rf "$SUBLIME/User/" #remove existing user folder
+sudo ln -s "$PWD/sublime_user/" "$SUBLIME/User" # link user folder from here
+#download packge control to Installed Packages
+sudo curl -o "$SUBLIME/../Installed Packages/Package Control.sublime-package" 'http://sublime.wbond.net/Package Control.sublime-package'
+
+echo "${GREEN}done${NC}"
