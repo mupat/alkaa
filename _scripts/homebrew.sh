73e360d7c052dab1dd79850cad9fecfc39734a6d
Mupat <dev@mupat.net>
2020-07-13 11:52:57 +0200

    Add install of mas

--- _scripts/homebrew.sh
+++ _scripts/homebrew.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../_helper.sh" #load helper
 
-#install homebrew
+# install homebrew
 cecho "Install homebrew" $GREEN
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 
-#install cask
+# install cask
 cecho "Install homebrew cask" $GREEN
 brew tap homebrew/cask-versions
 brew tap homebrew/cask-fonts
+
+# install mas (mac app stpre cli)
+cecho "Install MAS" $GREEN
+brew install mas
 
 cecho "Install actual git version" $GREEN
 brew install git
